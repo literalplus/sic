4a91eff4aa736a97a3da018d23ac7595de724416
xxyy <devnull@nowak-at.net>
2016-05-05 00:23:16 +0200

    Fix Flyway errors

--- src/main/resources/db/migration/V0001__Initial_schema.sql
+++ src/main/resources/db/migration/V0001__Initial_schema.sql
@@ -12,21 +12,6 @@ CREATE TABLE IF NOT EXISTS sic_person
   last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
 );
 
--- sic_quote table (stores quotes by person)
-CREATE TABLE sic_quote
-(
-  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  person_id INT NOT NULL,
-  text TEXT NOT NULL,
-  creation_date DATETIME NOT NULL,
-  last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
-  creator_name VARCHAR(255) DEFAULT NULL NULL,
-  CONSTRAINT sic_quote_sic_person_id_fk FOREIGN KEY (person_id) REFERENCES sic_person (id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-CREATE INDEX sic_quote_person_id_index ON sic_quote (person_id);
-ALTER TABLE sic_quote COMMENT = 'Stores sic quotes by person';
-
-
 -- This adds user and authority tables for use with Spring Security's default JDBC thing
 
 CREATE TABLE sic_user
@@ -44,3 +29,19 @@ CREATE TABLE sic_authority
   CONSTRAINT sic_authority_sic_user_username_fk FOREIGN KEY (username) REFERENCES sic_user (username) ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE UNIQUE INDEX sic_authority_username_authority_uindex ON sic_authority (username, authority);
+
+
+-- sic_quote table (stores quotes by person)
+CREATE TABLE sic_quote
+(
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  person_id INT NOT NULL,
+  text TEXT NOT NULL,
+  creation_date DATETIME NOT NULL,
+  last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+  creator_name VARCHAR(255) DEFAULT NULL NULL,
+  CONSTRAINT sic_quote_sic_person_id_fk FOREIGN KEY (person_id) REFERENCES sic_person (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT sic_quote_sic_user_creator_name_fk FOREIGN KEY (creator_name) REFERENCES sic_user (username) ON DELETE SET NULL ON UPDATE CASCADE
+);
+CREATE INDEX sic_quote_person_id_index ON sic_quote (person_id);
+ALTER TABLE sic_quote COMMENT = 'Stores sic quotes by person';
--- src/main/resources/db/migration/V0002__Field_creators.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE sic_quote ADD creator_name VARCHAR(50) DEFAULT NULL NULL;
-ALTER TABLE sic_quote
-ADD CONSTRAINT sic_quote_sic_user_username_fk
-FOREIGN KEY (creator_name) REFERENCES sic_user (username) ON DELETE SET NULL ON UPDATE CASCADE;
-ALTER TABLE sic_quote
-  MODIFY COLUMN creator_name VARCHAR(50) DEFAULT NULL AFTER text
