01d565f0301a3ebb85fd7cec1a5ba45b2f8df975
xxyy <devnull@nowak-at.net>
2016-05-20 19:11:02 +0200

    Add status parameter to JsonPropagatingException

--- src/main/java/li/l1t/sic/exception/JsonPropagatingException.java
+++ src/main/java/li/l1t/sic/exception/JsonPropagatingException.java
@@ -1,5 +1,8 @@
 package li.l1t.sic.exception;
 
+import org.apache.commons.lang3.Validate;
+import org.springframework.http.HttpStatus;
+
 /**
  * Exception class for propagating an error message that is returned
  * as JSON object to the client.
@@ -8,6 +11,8 @@ package li.l1t.sic.exception;
  * @since 2016-02-14
  */
 public class JsonPropagatingException extends RuntimeException {
+    private HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
     public JsonPropagatingException(String message) {
         super(message);
     }
@@ -18,5 +23,24 @@ public class JsonPropagatingException extends RuntimeException {
 
     public JsonPropagatingException(Throwable cause) {
         super(cause);
+    }
+
+    public JsonPropagatingException(String message, HttpStatus status) {
+        this(message, null, status);
+    }
+
+    public JsonPropagatingException(String message, Throwable cause, HttpStatus status) {
+        super(message, cause);
+        Validate.notNull(status, "status");
+        this.status = status;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        Validate.notNull(status, "status");
+        this.status = status;
     }
 }
